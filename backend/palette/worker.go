9243116a58bb6b2fb364016fdf7cde3e597d2b01
Sebastian Gilits <sep.gil@gmail.com>
2016-05-05 23:01:02 +0200

    Added function which calculates the actual color scheme.

---
+++ backend/palette/worker.go
@@ -0,0 +1,59 @@
+package palette
+
+import (
+	"fmt"
+	"github.com/mdesenfants/gokmeans"
+	"image"
+)
+
+// A worker calculates the 5 most dominant colors in an image based on the k-means algorithm.
+// github.com/mdesenfants/gokmeans is used to caculate the kmeans.
+
+// MAX_HEIGHT defines maximal height to which the an image will be resized for the k-means calculation.
+const MAX_HEIGHT = 200
+
+// Palette is a slice with the dominant colors for an image.
+type Palette [5][3]int
+
+// calcPalette returns the dominant colors the given image.
+func calcPalette(img image.Image) (Palette, error) {
+	var palette Palette
+
+	w := img.Bounds().Max.X - img.Bounds().Min.X
+	h := img.Bounds().Max.Y - img.Bounds().Min.Y
+
+	// Create a slice of observations, with the size of the image
+	observations := make([]gokmeans.Node, w*h)
+
+	// The k-means lib needs a continuous slice of observations(in our case pixels) instead of two dimensional
+	// slice. This loop creates this slice, idx keeps the index for the observations.
+	idx := 0
+	for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+		for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+			// Get the pixel at the current location, ignore any alpha value.
+			r, g, b, _ := img.At(x, y).RGBA()
+
+			// We only work with 8 bit per color for now.
+			rF := float64(r) / 255.0
+			gF := float64(g) / 255.0
+			bF := float64(b) / 255.0
+
+			observations[idx] = gokmeans.Node{rF, gF, bF}
+			idx++
+		}
+	}
+
+	// Calculate 5 centroids of the observations, which are in our case are the dominant colors in max. 10 rounds.
+	success, centroids := gokmeans.Train(observations, 5, 10)
+
+	if !success {
+		return palette, fmt.Errorf("Could not calculate centroids.")
+	}
+
+	for idx, node := range centroids {
+		palette[idx][0] = uint8(node[0])
+		palette[idx][1] = uint8(node[1])
+		palette[idx][2] = uint8(node[2])
+	}
+	return palette, nil
+}
