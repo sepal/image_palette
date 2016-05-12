5758c8bc8d3bd697d6543f1ee05b4af04af62f3a
Sebastian Gilits <sep.gil@gmail.com>
2016-05-12 20:19:41 +0200

    Improved the upload component on the frontend.

--- frontend/src/app/upload/upload.component.ts
+++ frontend/src/app/upload/upload.component.ts
@@ -1,39 +1,8 @@
 import {Component} from '@angular/core';
 
 @Component({
-    styles: [`
-    img {
-        max-width: 960px;
-        height: auto;
-    }
-    .color {
-        display: inline-block;
-        width: 100px;
-        height: 100px;
-        margin: 5px;
-        text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.8);
-        
-    }
-    
-    .color span {
-        height: 100px;
-        line-height: 100px;
-    }
-    `],
-    template: `
-    <div *ngIf="image_submitted == false">
-        <input (change)="fileSelected($event)" type="file"/>
-        <input (click)="uploadImage($event)" type="submit" />
-    </div>
-    <div *ngIf="image_submitted == true">
-        <div *ngFor="let color of colors" class="color" [style.background-color]="color">
-          <span>{{color}}</span>
-        </div>
-    </div>
-    <div *ngIf="image_src != null">
-        <img class="preview" src={{image_src}} alt={{image_name}}>
-    </div>
-    `
+    styles: [require('./upload.component.scss')],
+    template: require('./upload.component.html')
 })
 export class UploadComponent {
     image: File;
@@ -45,21 +14,20 @@ export class UploadComponent {
 
 
     constructor() {
-        this.image_submitted = false
+        this.image_submitted = false;
     }
 
     uploadImage() {
-        this.makeFileRequest('http://localhost:8000/upload', [], this.image).then((result) => {
-            this.server_response = result.id;
+        this.makeFileRequest('http://localhost:8000/upload', [], this.image).then((result: any) => {
             this.image_submitted = true;
 
             let ws: WebSocket = new WebSocket(`ws://localhost:8000/image/${result.id}`);
             ws.onmessage = (event) => {
                 let img = JSON.parse(event.data);
                 this.colors = img.colors;
-            }
+            };
         }).catch((err) => {
-            console.error("Error while uploading image:");
+            console.error('Error while uploading image:');
             console.error(err);
         });
     }
@@ -67,17 +35,17 @@ export class UploadComponent {
     fileSelected(event) {
         let file: File = event.target.files[0];
         if (event.target.files.length < 1) {
-            return
+            return;
         }
         if (file.type !== 'image/jpeg' && file.type !== 'image/png') {
             alert('Wrong file!');
             this.image_src = null;
-            return
+            return;
         }
         let reader: FileReader = new FileReader();
 
-        reader.addEventListener("load", (event) => {
-            this.image_src = event.target.result;
+        reader.addEventListener('load', (e: any) => {
+            this.image_src = e.target.result;
             this.image_name = this.image.name;
         }, false);
         this.image = file;
@@ -90,11 +58,11 @@ export class UploadComponent {
         return new Promise((resolve, reject) => {
             let formData: any = new FormData();
             let xhr = new XMLHttpRequest();
-            formData.append("image", file, file.name);
+            formData.append('image', file, file.name);
 
             xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
                         resolve(JSON.parse(xhr.response));
                     } else {
                         reject(xhr.response);
@@ -102,7 +70,7 @@ export class UploadComponent {
                 }
             };
 
-            xhr.open("POST", url, true);
+            xhr.open('POST', url, true);
             xhr.send(formData);
         });
     }
